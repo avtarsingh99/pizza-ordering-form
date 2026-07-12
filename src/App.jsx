83202e5c5604318c35eb54dbeca87d516486b5e4
Avtar <avtarsingh160701@gmail.com>
2026-07-12 18:13:54 +0530

    folder str created

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <header>
+        <h1>The Pizza Factory</h1>
+        <p className='tagline'>The Art of Great Pizza</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
