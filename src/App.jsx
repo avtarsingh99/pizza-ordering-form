c06731400693ae3a13dcb0d00874f6e3173b2d9a
Avtar <avtarsingh160701@gmail.com>
2026-07-18 16:58:39 +0530

    validation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,105 @@
 import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import './App.css'
 
 function App() {
 
-  const [customerInfo, setCustomerInfo] = useState({
-    name: '',
-    email: '',
-    phone: '',
-    gender: '',
-    termsAndConditions: false,
-    address: '',
-    orderType: ''
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      phone: '',
+      gender: '',
+      whatsappUpdates: false,
+      address: '',
+      orderType: '',
+      size: 'medium',
+      crust: 'normal',
+      toppings: [],
+      coke: 0
+    }
   })
 
-  const [pizzaOrder, setPizzaOrder] = useState({
-    size: 'medium',
-    crust: 'normal',
-    toppings: [],
-    coke: 0
-  })
+  const coke = watch('coke')
 
-  const [formState, setFormState] = useState({
-    erros: {},
-    isSubmitting: false
-  })
+  const pizzaSizePrices = {
+    small: 100,
+    medium: 200,
+    large: 300,
+    xlarge: 500
+  }
 
-  const toppingsOptions = ['paneer', 'mushroom', 'corn', 'capsicum', 'olives', 'jalapeno', 'onion', 'soyachunks', 'pineapple']
+  const crustPrices = {
+    normal: 0,
+    thin: 100,
+    thick: 200,
+    stuffed: 300
+  }
+
+  const getTotalAmount = () => {
+    let total = 0;
+    // console.log("Pizza order: ", pizzaOrder)
+    // adding price of pizza size
+
+    total += pizzaSizePrices[watch('size')];
+
+    // adding the price of crust
+
+    total += crustPrices[watch('crust')];
+
+    // adding the price of toppings
+
+    const toppingsPrice = 50 * watch('toppings').length;
+
+    total += toppingsPrice;
+
+    // adding the price of coke
+
+    if (coke >= 1) {
+      total += (coke * 40)
+    }
+
+    return total;
+  }
+
+  const GSTCharges = () => {
+    return Math.floor(18 / 100 * getTotalAmount());
+  }
+
+  const handleOrder = (data) => {
+    const total = getTotalAmount() + GSTCharges();
+    alert(
+      `🍕 Order Placed Successfully! 🍕
+      --------------------------------
+      Name: ${data.name}
+      Phone: ${data.phone}
+      Email: ${data.email}
+      
+      Order Type: ${data.orderType === 'homeDelivery' ? 'Home Delivery' : 'In-Store Pickup'}
+      ${data.orderType === 'homeDelivery' ? `Address: ${data.address}` : ''}
+      
+      Pizza: ${data.size.charAt(0).toUpperCase() + data.size.slice(1)} Size, ${data.crust.charAt(0).toUpperCase() + data.crust.slice(1)} Crust
+      Toppings: ${data.toppings.length > 0 ? data.toppings.map(t => t.charAt(0).toUpperCase() + t.slice(1)).join(', ') : 'None'}
+      ${data.coke > 0 ? `Coke: ${data.coke} x ₹40` : ''}
+      
+      --------------------------------
+      Total (incl. GST): ₹ ${total}
+      --------------------------------
+      Thank you for ordering! 🎉`
+    );
+
+    reset();
+    // console.log("Submitting: ", data)
+  }
+
+  const toppingsOptions = ['paneer', 'mushroom', 'corn', 'capsicum', 'olives', 'jalapeno', 'onion', 'soyachunks']
 
   return (
     <div className='app'>
@@ -33,7 +107,7 @@ function App() {
         <h1>The Pizza Factory</h1>
         <p className='tagline'>The Art of Great Pizza</p>
       </header>
-      <main>
+      <form onSubmit={handleSubmit(handleOrder)}>
         <h2>Pizza Order Form</h2>
         <section className='customer-info'>
           <h3>Customer Information</h3>
@@ -41,103 +115,123 @@ function App() {
             <div className='form-group'>
               <label htmlFor='customer-name'>Name: </label>
               <input
-                type='text'
                 id='customer-name'
-                name='name'
-                placeholder='e.g. Rahul Singh'
-                value={customerInfo.name}
-                onChange={(e) => setCustomerInfo({ ...customerInfo, name: e.target.value })}
+                {...register('name',
+                  {
+                    required: { value: true, message: "Name is required" },
+                    minLength: { value: 3, message: "Name must be atleast of 3 characters" },
+                    maxLength: { value: 20, message: "Name can't be more than 20 characters" },
+                    pattern: { value: /^[A-Za-z ]+$/, message: "Characters 0 to 9 & special characters like !,@,#,$ aren't allowed" }
+                  })}
+                placeholder='Enter name here'
+                style={errors.name ? { border: '1px solid red' } : {}}
               />
+              {errors.name && <p style={{ color: 'red', margin: 0, fontSize: '12px' }}>{errors.name.message}</p>}
             </div>
             <div className='form-group'>
               <label htmlFor='customer-phone'>Phone: </label>
               <input
-                type='tel'
                 id='customer-phone'
-                name='phone'
+                {...register('phone',
+                  {
+                    required: { value: true, message: "Phone number is required" },
+                    pattern: { value: /^[0-9]{10}$/, message: "Phone number must be exactly 10 digits" }
+                  })}
                 placeholder='e.g. 123 4567 890'
-                value={customerInfo.phone}
-                onChange={(e) => setCustomerInfo({ ...customerInfo, phone: e.target.value })}
+                style={errors.phone ? { border: '1px solid red' } : {}}
               />
+              {errors.phone && <p style={{ color: 'red', margin: 0, fontSize: '12px' }}>{errors.phone.message}</p>}
             </div>
             <div className='form-group'>
               <label htmlFor='customer-email'>Email: </label>
               <input
-                type='email'
                 id='customer-email'
-                name='email'
+                {...register('email',
+                  {
+                    required: { value: true, message: "Email address is required" },
+                    pattern: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, message: "Please enter a valid email address" }
+                  })}
                 placeholder='e.g. abc@example.com'
-                value={customerInfo.email}
-                onChange={(e) => setCustomerInfo({ ...customerInfo, email: e.target.value })}
+                style={errors.email ? { border: '1px solid red' } : {}}
               />
+              {errors.email && <p style={{ color: 'red', margin: 0, fontSize: '12px' }}>{errors.email.message}</p>}
             </div>
             <div className='checkbox-group'>
               <input
                 type='checkbox'
-                name='termsAndConditions'
                 id='user-check'
-                value={customerInfo.termsAndConditions}
-                onChange={(e) => setCustomerInfo({ ...customerInfo, termsAndConditions: e.target.checked })}
+                {...register('whatsappUpdates')}
               />
               <label htmlFor='user-check'>I agree to receive updates via WhatsApp</label>
             </div>
             <div className='radio-group'>
-              <fieldset>
+              <fieldset style={errors.gender ? { border: '1px solid red' } : {}}>
                 <legend>Gender: </legend>
                 <label>
                   <input
                     type='radio'
-                    name='gender'
                     value='male'
-                    checked={customerInfo.gender === 'male'}
-                    onChange={(e) => setCustomerInfo({ ...customerInfo, gender: e.target.value })}
+                    {...register('gender',
+                      {
+                        required: { value: true, message: "Please select a gender" }
+                      })}
                   />
                   Male
                 </label>
                 <label>
                   <input
                     type='radio'
-                    name='gender'
                     value='female'
-                    checked={customerInfo.gender === 'female'}
-                    onChange={(e) => setCustomerInfo({ ...customerInfo, gender: e.target.value })}
+                    {...register('gender',
+                      {
+                        required: { value: true, message: "Please select a gender" }
+                      })}
                   />
                   Female
                 </label>
               </fieldset>
-              <fieldset>
+              {errors.gender && <p style={{ color: 'red', margin: 0, fontSize: '12px' }}>{errors.gender.message}</p>}
+              <fieldset style={errors.orderType ? { border: '1px solid red' } : {}}>
                 <legend>Order Type:</legend>
                 <label>
                   <input
                     type='radio'
-                    name='orderType'
                     value='homeDelivery'
-                    checked={customerInfo.orderType === 'homeDelivery'}
-                    onChange={(e) => setCustomerInfo({ ...customerInfo, orderType: e.target.value })}
+                    {...register('orderType',
+                      {
+                        required: { value: true, message: "Please select order type" }
+                      })}
                   />
                   Home Delivery
                 </label>
                 <label>
                   <input
                     type='radio'
-                    name='orderType'
                     value='inStorePickup'
-                    checked={customerInfo.orderType === 'inStorePickup'}
-                    onChange={(e) => setCustomerInfo({ ...customerInfo, orderType: e.target.value })}
+                    {...register('orderType',
+                      {
+                        required: { value: true, message: "Please select order type" }
+                      })}
                   />
                   In-Store Pickup
                 </label>
               </fieldset>
+              {errors.orderType && <p style={{ color: 'red', margin: 0, fontSize: '12px' }}>{errors.orderType.message}</p>}
             </div>
-            {customerInfo.orderType === 'homeDelivery' && (
+            {watch('orderType') === 'homeDelivery' && (
               <div className='form-group'>
                 <label htmlFor='customer-address'>Address: </label>
                 <textarea
+                  style={errors.address ? { border: '1px solid red' } : {}}
                   id='customer-address'
-                  value={customerInfo.address}
-                  onChange={(e) => setCustomerInfo({ ...customerInfo, address: e.target.value })}
+                  placeholder='e.g. 123 your street/area/locality landmark'
+                  {...register('address',
+                    {
+                      required: { value: true, message: "Address is required for home delivery" }
+                    })}
                   rows={4}
                 />
+                {errors.address && <p style={{ color: 'red', margin: 0, fontSize: '12px' }}>{errors.address.message}</p>}
               </div>
             )}
           </div>
@@ -149,87 +243,108 @@ function App() {
               <label htmlFor='pizza-size'>Size: </label>
               <select
                 id='pizza-size'
-                value={pizzaOrder.size}
-                onChange={(e) => setPizzaOrder({ ...pizzaOrder, size: e.target.value })}
+                {...register('size')}
               >
-                <option value='small'>Small</option>
-                <option value='medium'>Medium</option>
-                <option value='large'>Large</option>
-                <option value='xlarge'>Extra Large</option>
+                <option value='small'>Small - ₹ {pizzaSizePrices['small']}</option>
+                <option value='medium'>Medium - ₹ {pizzaSizePrices['medium']}</option>
+                <option value='large'>Large  -  ₹ {pizzaSizePrices['large']}</option>
+                <option value='xlarge'>Extra Large - ₹ {pizzaSizePrices['xlarge']}</option>
               </select>
             </div>
             <div className='form-group'>
               <label htmlFor='pizza-crust'>Crust: </label>
               <select
                 id='pizza-crust'
-                value={pizzaOrder.crust}
-                onChange={(e) => setPizzaOrder({ ...pizzaOrder, crust: e.target.value })}
+                {...register('crust')}
               >
                 <option value='normal'>Normal</option>
-                <option value='thin'>Thin</option>
-                <option value='thick'>Thick</option>
-                <option value='stuffed'>stuffed</option>
+                <option value='thin'>Thin - ₹ {crustPrices['thin']} extra</option>
+                <option value='thick'>Thick - ₹ {crustPrices['thick']} extra</option>
+                <option value='stuffed'>Stuffed - ₹ {crustPrices['stuffed']} extra</option>
               </select>
             </div>
             <div className='form-group'>
-              <fieldset>
-                <legend>Toppings: </legend>
+              <fieldset style={errors.toppings ? { border: '1px solid red' } : {}}>
+                <legend>Toppings (₹50 per topping): </legend>
                 <div className='toppings-grid'>
-                  {toppingsOptions.map(topping => (
+                  {toppingsOptions.map((topping, index) => (
                     <div key={topping} className='topping'>
                       <input
                         type='checkbox'
                         value={topping}
+                        {...register('toppings',
+                          {
+                            validate: (value) => (value && value.length > 0) || "Please select at least one topping"
+                          })}
                       />
                       <label>{topping.charAt(0).toUpperCase() + topping.slice(1)}</label>
                     </div>
                   ))}
                 </div>
               </fieldset>
+              {errors.toppings && <p style={{ color: 'red', margin: 0, fontSize: '12px' }}>{errors.toppings.message}</p>}
             </div>
-            <h4>Drinks (as per the MRP)</h4>
+            <h4>Side Drinks</h4>
             <div className='drinks-group'>
               <div className='left-part'>
-                <p>Coke (350mL)</p>
+                <p>Coke (350mL - MRP ₹40 )</p>
               </div>
               <div className='right-part'>
-                <button onClick={() => setPizzaOrder({ ...pizzaOrder, coke: pizzaOrder.coke + 1 })}>+</button>
-                <p>{pizzaOrder.coke}</p>
-                <button onClick={() => pizzaOrder.coke > 0 ? setPizzaOrder({ ...pizzaOrder, coke: pizzaOrder.coke - 1 }) : alert("Quantity can't be negative!")}>-</button>
+                <button type='button' onClick={() => coke < 20 ? setValue('coke', coke + 1) : alert("Can't order more than 20 coke at once!")}>+</button>
+                <p>{watch('coke')}</p>
+                <button type='button' onClick={() => coke > 0 ? setValue('coke', coke - 1) : alert("Quantity can't be negative!")}>-</button>
               </div>
             </div>
           </div>
         </section>
         <section className='summary'>
-          <h3>Summary</h3>
+          <div className='total-price'>
+            <strong>Summary</strong>
+            <button className='pay-now' type='submit'>Order Now | Total - <strong>₹ {getTotalAmount() + GSTCharges()}</strong></button>
+          </div>
           <div className='item-list'>
             <div className='item'>
               <div className='list-item'>
-                {pizzaOrder.size.charAt(0).toUpperCase() + pizzaOrder.size.slice(1)} Size Pizza ({pizzaOrder.crust.charAt(0).toUpperCase() + pizzaOrder.crust.slice(1)} Crust)
+                {watch('size').charAt(0).toUpperCase() + watch('size').slice(1)} Size Pizza ({watch('crust').charAt(0).toUpperCase() + watch('crust').slice(1)} Crust)
               </div>
               <div className='item-price'>
-                ₹ 350
+                ₹ {pizzaSizePrices[watch('size')] + crustPrices[watch('crust')]}
               </div>
             </div>
+            <div className='item'>
+              <div className='toppings-item'>
+                <p>Toppings:</p>
+                <div className='toppings-bracket'>
+                  [{watch('toppings').map((t, i) => (
+                    <p key={t}>{t.charAt(0).toUpperCase() + t.slice(1) + (i === watch('toppings').length - 1 ? "" : ",")}</p>
+                  ))}]
+                </div>
+              </div>
+              <div className='item-price'>
+                ₹ {watch('toppings').length * 50}
+              </div>
+            </div>
+            {coke >= 1 && (
+              <div className='item'>
+                <div className='list-item'>
+                  Coke (350mL) x {coke}
+                </div>
+                <div className='item-price'>
+                  ₹ {coke * 40}
+                </div>
+              </div>
+            )}
             <div className='item'>
               <div className='list-item'>
                 GST (18%)
               </div>
               <div className='item-price'>
-                ₹ 50
+                ₹ {GSTCharges()}
               </div>
             </div>
           </div>
-          <div className='item'>
-            <div className='list-item'>
-              Grand Total
-            </div>
-            <div className='item-price'>
-              ₹ 400
-            </div>
-          </div>
         </section>
-      </main>
+      </form>
     </div>
   )
 }
